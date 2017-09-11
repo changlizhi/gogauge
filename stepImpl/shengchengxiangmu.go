71fb2943a2829bb3f569da7ee43a0d4f7f1a14e2
changlz <changlizhi@aliyun.com>
2017-09-11 16:47:31 +0800

    生成main

--- stepImpl/shengchengxiangmu.go
+++ stepImpl/shengchengxiangmu.go
@@ -3,8 +3,13 @@ package stepImpl
 import (
 	"github.com/getgauge-contrib/gauge-go/gauge"
 	"jichu/scmoxing"
+	"jichu/scmain"
 )
 
 var _ = gauge.Step("生成moxings", func() {
 	scmoxing.Shengchengmoxings()
+})
+
+var _ = gauge.Step("生成Main", func() {
+	scmain.Shengchengmain()
 })
